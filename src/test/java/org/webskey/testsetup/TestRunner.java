111b68da3403bd0656d016318b7f21998023e2c9
Webskey <jolo_92@wp.pl>
2019-05-24 15:40:36 +0200

    -update project structure

--- src/test/java/org/webskey/testsetup/TestRunner.java
+++ src/test/java/org/webskey/testsetup/TestRunner.java
@@ -8,7 +8,8 @@ import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(plugin = {"pretty"})
+@CucumberOptions(plugin = {"pretty"},
+features = {"classpath:features/"})
 public class TestRunner {
 	
 	@AfterClass
